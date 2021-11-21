3fd5ed47b18e0f6402e24c43dbbbb96f5486d6bb
David Francoeur <dfrancoeur04@gmail.com>
2021-11-20 20:24:41 -0500

    chore: mv resource file

--- back-end/src/main/scala/depviz/html/Instances.scala
+++ back-end/src/main/scala/depviz/html/Instances.scala
@@ -4,12 +4,12 @@ import org.http4s.EntityEncoder
 import org.http4s.headers.`Content-Type`
 import org.http4s.MediaType
 import scalatags.Text
+import org.http4s.Charset
 
 object instances {
-  implicit def htmlEntityEncoder[F[_]]
-      : EntityEncoder[F, Text.TypedTag[String]] =
+  implicit def htmlEntityEncoder[F[_]]: EntityEncoder[F, Text.TypedTag[String]] =
     EntityEncoder
       .stringEncoder[F]
       .contramap[Text.TypedTag[String]](_.toString)
-      .withContentType(`Content-Type`(MediaType.text.html))
+      .withContentType(`Content-Type`(MediaType.text.html).withCharset(Charset.`UTF-8`))
 }
